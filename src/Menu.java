fb0ef9a264bdb01477653078a2ac5556601ec0a8
Christophe Baillet <christophe.baillet@le-campus-numerique.fr>
2022-11-09 16:52:32 +0100

    add quit function to start menu

--- src/Menu.java
+++ src/Menu.java
@@ -4,7 +4,7 @@ public class Menu {
     private final Scanner scanner = new Scanner(System.in);
 
     public Personnage start() {
-        System.out.println("[1] create a character | [2] exit the game");
+        System.out.println("[1] create a character | [2] Quit");
         String userChoice = scanner.nextLine();
         if (userChoice.equals("1")) {
             return createCharacter();
@@ -19,34 +19,41 @@ public class Menu {
     }
 
     public Personnage createCharacter() {
-        String userClass = warriorOrMagician(askClass());
+        String userClass = askClass();
         String characterName = askCharacterName();
         return validateCharacter(new Personnage(userClass, characterName));
     }
 
 
-    public String warriorOrMagician(String className) {
-        if (className.equals("1") || className.equals("2")) {
-            if (className.equals("1")){
+    public String warriorOrMagician(String userInput) {
+        if (userInput.equals("1") || userInput.equals("2")) {
+            if (userInput.equals("1")){
                 return "Warrior";
             }else {
                 return "Magician";
             }
-        } else {
+        }else if (userInput.equals("q")){
+            exit();
+            return null;
+        }
+        else {
             return askClass();
         }
     }
 
     public String askClass() {
-        System.out.println("[1] Warrior |[2] Magician");
+        System.out.println("[1] Warrior |[2] Magician |[q] Quit");
         String userInput = scanner.nextLine();
-        System.out.println(userInput);
         return warriorOrMagician(userInput);
     }
 
     public String askCharacterName() {
-        System.out.println("What is the name of you character ? ");
-        return scanner.nextLine();
+        System.out.println("What is the name of you character ? | [q] Quit");
+        String userInput = scanner.nextLine();
+        if (userInput.equals("q")){
+            exit();
+        }
+        return userInput;
     }
 
     public void showCharacterInfos(Personnage character) {
@@ -56,17 +63,19 @@ public class Menu {
     public Personnage validateCharacter(Personnage character) {
         System.out.println("Here is your character :");
         System.out.println("class :" + character.getClasse() + " | name :" + character.getName());
-        System.out.println("[1] validate | [2] modify");
+        System.out.println("[1] validate | [2] modify | [q] Quit");
         String userChoice = scanner.nextLine();
-        if (!userChoice.equals("1")) {
-            System.out.println("What do you want to modify ? | [1] name  | [2] class");
+        if (userChoice.equals("2")) {
+            System.out.println("What do you want to modify ? | [1] name  | [2] class | [q] Quit");
             userChoice = scanner.nextLine();
-            if (userChoice.equals("1")) {
-                character.setName(askCharacterName());
-            } else {
-                character.setClasse(askClass());
+            switch (userChoice) {
+                case "1" -> character.setName(askCharacterName());
+                case "2" -> character.setClasse(askClass());
+                case "q" -> exit();
             }
             validateCharacter(character);
+        }else if (userChoice.equals("q")){
+            exit();
         }
         return character;
     }
