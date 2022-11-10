32d3a4b360688951714bc49dc81af7b7eb19356a
Christophe Baillet <christophe.baillet@le-campus-numerique.fr>
2022-11-10 16:58:28 +0100

    refacto ping pong du code avec le menu qui affiche et qui renvoie les inputs de l'utilisateurs et game qui gère les menus

--- src/Game.java
+++ src/Game.java
@@ -3,13 +3,75 @@ import java.util.Scanner;
 public class Game {
     private final Menu menu = new Menu();
     private final Scanner scanner = new Scanner(System.in);
+    private Personnage character;
 
     public void main() {
         start();
     }
-    public void start(){
-        Personnage character = menu.start();
+    private void start(){
+        String userInput = menu.main();
+        switch (userInput){
+            case "1" -> character = createCharacter();
+            case "2" -> character = loadCharacter();
+            case "q" -> exit();
+        }
+        System.out.println(character);
     }
 
+    private Personnage loadCharacter(){
+        return new Personnage();
+    }
 
+    private Personnage createCharacter(){
+        String userClass = warriorOrMagician(menu.askClass());
+        String userName = menu.askCharacterName();
+        return validateCharacter(new Personnage(userClass, userName));
+    }
+
+    private void exit(){
+        System.exit(0);
+    }
+
+    private String warriorOrMagician(String userInput) {
+        switch (userInput){
+            case "1" -> userInput = "Warrior";
+            case "2" -> userInput = "Magician";
+            case "q" -> exit();
+            default -> warriorOrMagician(menu.askClass());
+        }
+        return userInput;
+    }
+
+    private Personnage validateCharacter(Personnage character) {
+        menu.showCharacterNameAndClass(character);
+        String userChoice = menu.askValidationOfCharacter();
+        switch (userChoice) {
+            case "1" -> validate();
+            case "2" -> validateCharacter(modify(character));
+            case "q" -> exit();
+            default -> validateCharacter(character);
+        }
+        return character;
+    }
+
+    private void validate(){
+
+    }
+
+    private Personnage modify(Personnage character){
+        String userInput = menu.modifyCharacter();
+        switch (userInput){
+            case "1" -> character.setClasse(menu.askClass());
+            case "2" -> character.setName(menu.askCharacterName());
+            case "3" -> setClassAndName(character);
+            case "q" -> exit();
+            default -> modify(character);
+        }
+        return character;
+    }
+
+    private void setClassAndName(Personnage character){
+        character.setClasse(menu.askClass());
+        character.setName(menu.askCharacterName());
+    }
 }
--- src/Menu.java
+++ src/Menu.java
@@ -3,80 +3,35 @@ import java.util.Scanner;
 public class Menu {
     private final Scanner scanner = new Scanner(System.in);
 
-    public Personnage start() {
-        System.out.println("[1] create a character | [2] Quit");
-        String userChoice = scanner.nextLine();
-        if (userChoice.equals("1")) {
-            return createCharacter();
-        } else {
-            exit();
-            return null;
-        }
-
+    public String main(){
+        return askQuestion("[1] Create character | [2] Load character | [q] Quit");
     }
-    public void exit() {
-        System.exit(0);
-    }
-
-    public Personnage createCharacter() {
-        String userClass = askClass();
-        String characterName = askCharacterName();
-        return validateCharacter(new Personnage(userClass, characterName));
-    }
-
-
-    public String warriorOrMagician(String userInput) {
-        if (userInput.equals("1") || userInput.equals("2")) {
-            if (userInput.equals("1")){
-                return "Warrior";
-            }else {
-                return "Magician";
-            }
-        }else if (userInput.equals("q")){
-            exit();
-            return null;
-        }
-        else {
-            return askClass();
-        }
-    }
-
     public String askClass() {
-        System.out.println("[1] Warrior |[2] Magician |[q] Quit");
-        String userInput = scanner.nextLine();
-        return warriorOrMagician(userInput);
+        return askQuestion("[1] Warrior |[2] Magician |[q] Quit");
     }
 
     public String askCharacterName() {
-        System.out.println("What is the name of you character ? | [q] Quit");
-        String userInput = scanner.nextLine();
-        if (userInput.equals("q")){
-            exit();
-        }
-        return userInput;
+        return askQuestion("What is the name of you character ? | [q] Quit");
+    }
+
+    public String askValidationOfCharacter(){
+        return askQuestion("[1] validate | [2] modify | [q] Quit");
     }
 
     public void showCharacterInfos(Personnage character) {
         System.out.println(character);
     }
-
-    public Personnage validateCharacter(Personnage character) {
+    public void showCharacterNameAndClass(Personnage character){
         System.out.println("Here is your character :");
         System.out.println("class :" + character.getClasse() + " | name :" + character.getName());
-        System.out.println("[1] validate | [2] modify | [q] Quit");
-        String userChoice = scanner.nextLine();
-        if (userChoice.equals("2")) {
-            System.out.println("What do you want to modify ? | [1] name  | [2] class | [q] Quit");
-            userChoice = scanner.nextLine();
-            switch (userChoice) {
-                case "1" -> character.setName(askCharacterName());
-                case "2" -> character.setClasse(askClass());
-                case "q" -> exit();
-            }
-            validateCharacter(character);
-        }else if (userChoice.equals("q")){
-            exit();
-        }
-        return character;
+    }
+
+    private String askQuestion(String question){
+        System.out.println(question);
+        return scanner.nextLine();
+    }
+
+    public String modifyCharacter(){
+        return askQuestion("What do you want to modify ? | [1] class | [2] name | [3] Both | [q] Quit");
     }
 }
